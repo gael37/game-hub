f4f30984ba914cc01cb22f025b71580b69680311
gael37 <gx.duchesne@gmail.com>
2024-01-26 22:27:32 +0000

    Refactoring- create a reusable APIClient

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,8 +1,5 @@
 import { useQuery } from "@tanstack/react-query"
-import genres from "../data/genres"
-import apiClient from "../services/api-client"
-import { Genre } from "./useGenres"
-import { FetchResponse } from "../services/api-client"
+import APIClient from "../services/api-client"
 
 export interface Platform {
   id: number,
@@ -10,13 +7,12 @@ export interface Platform {
   slug: string
 }
 
-// const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+const apiClient = new APIClient<Platform>('/platforms/lists/parents')
+
 
 const usePlatforms = () => useQuery({
   queryKey: ['platforms'],
-  queryFn: () =>
-    apiClient
-      .get<FetchResponse<Platform>>('/platforms/lists/parents').then(res => res.data),
+  queryFn: apiClient.getAll,
   staleTime: 24 * 60 * 60 * 1000, // 24h
   // initialData: { count: genres.length, results: genres },
 })
